b160736dfa9f6c1facb48a75daafe3309ab8c4f0
Luis J Pinto <otzi122@gmail.com>
2022-07-21 00:32:40 -0400

    feat: install plugin base

---
+++ plugins/amalgama/includes/class-amalgama-deactivator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * @link       https://www.linkedin.com/in/ljpinto/
+ * @since      1.0.0
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @author     Luis J Pinto <otzi122@gmail.com>
+ */
+class Amalgama_Deactivator
+{
+	/**
+	 * Short Description. (use period).
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate(): void
+	{
+	}
+}
